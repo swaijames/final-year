6d1c47cd7131dbfc7d87c53c2e0a7829bd544244
swaijames <swaijames255@gmail.com>
2022-05-26 19:07:45 +0300

    26 may

--- project_final/settings.py
+++ project_final/settings.py
@@ -57,6 +57,7 @@ ROOT_URLCONF = 'project_final.urls'
 
 TEMPLATES = [
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [BASE_DIR / 'templates']
         ,
