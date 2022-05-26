1f05eb476fc84d1ca922ed9454df3f9c2a92b8ad
sammoh <manasesamwel@gmail.com>
2022-05-26 09:04:01 -0700

    Thursday 26

--- project_final/settings.py
+++ project_final/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/media/"
 
 # email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
